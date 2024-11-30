e12cd6f50777e8f820d43394dd83a1e3999959c5
Supanat <k.suphanuch23@gmail.com>
2024-11-30 14:49:57 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int j=0;
+    int k=0;
+    int N;
+    while (true){
+     cout << "Enter an integer: ";
+     cin >> N;
+     if(N==0){
+         break;
+     }
+     if(N%2==0){
+         j=j+1;
+     }
+     else{
+         k=k+1;
+     }
+    }
+    cout << "#Even numbers = " << j << "\n";
+    cout << "#Odd numbers = " << k;
     return 0;
 }
